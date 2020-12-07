829c4d6ea49fe16f46f942ea2f2e164329009e6d
Derrick White <derrickswhite@outlook.com>
2020-12-07 17:18:38 -0500

    Adding progress

--- js/weather.js
+++ js/weather.js
@@ -4,21 +4,29 @@ function gettingJSON(){
 
     //Set default location if one isn't provided
     let location;
-
+    location = document.querySelector("#location").value;
+    if (location == "") {
+        location = "Ann Arbor";
+    }
     // Your code here.
     console.log("Location is : " + location);
 
     //set default temperature format if one isn't provided
-    let fomat;
-
+    let format;
     // Your code here.
-    console.log("Format is " + format);
+    format = document.querySelector("input[name=temp]:checked");
+    if (format.value == null) {
+        format = "imperial";
+    }
+    console.log("Format is " + format.value);
 
     //set the query  
     let query;
     // Your code here.  
-    
-    console.log("Query is :" + query);
+    query = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=0505b57b02b2704fe78431037b81302e";
+    //let api_key;
+    //api_key = "0505b57b02b2704fe78431037b81302e";
+    //console.log("Query is :" + query);
 
     //Create and set variables for each of the elements you
     //need to update, location, temp, the image, etc.
@@ -34,6 +42,6 @@ function gettingJSON(){
         //elements in HTML.  
         //I would print the JSON to the console
         // Your code here.
-
+        //console.log(JSON.stringify(json));
     });
 }
