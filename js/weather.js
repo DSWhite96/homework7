815ec84c153375671fb787b7f79d5a76e2d44e90
Derrick White <derrickswhite@outlook.com>
2020-12-07 20:07:36 -0500

    Final version

--- js/weather.js
+++ js/weather.js
@@ -8,7 +8,7 @@ function gettingJSON(){
         location = document.querySelector("#location").value;
     }
     else {
-        location = "Ann Arbor, USA"
+        location = "Ann Arbor";
     }
     // Your code here.
     console.log("Location is : " + location);
@@ -16,23 +16,18 @@ function gettingJSON(){
     //set default temperature format if one isn't provided
     let format;
     // Your code here.
-    format = document.querySelector("input[name=temp]:checked");
-    if (!format) {
-        //console.log("Format is " + format.value);
-        format = "imperial";
-        console.log("Format is " + format);
+    if (document.querySelector("input[name=temp]:checked")) {
+        format = document.querySelector("input[name=temp]:checked").value;
     }
     else {
-        console.log("Format is " + format.value); 
+        format = "imperial";
     }
+    console.log("Format is " + format);
 
     //set the query  
     let query;
     // Your code here.  
-    query = "api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=0505b57b02b2704fe78431037b81302e";
-    //let api_key;
-    //api_key = "0505b57b02b2704fe78431037b81302e";
-    //console.log("Query is :" + query);
+    query = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=" + format + "&appid=0505b57b02b2704fe78431037b81302e";
 
     //Create and set variables for each of the elements you
     //need to update, location, temp, the image, etc.
@@ -41,7 +36,9 @@ function gettingJSON(){
     let temp;
     let tempImg;
     // Your code here.
-
+    loc = document.querySelector("#loc");
+    temp = document.querySelector("#temp");
+    tempImg = document.querySelector("#tempImg");
 
     $.getJSON(query,function(json){
         //Use returned json to update the values of the three 
@@ -49,5 +46,12 @@ function gettingJSON(){
         //I would print the JSON to the console
         // Your code here.
         console.log(JSON.stringify(json));
+        loc.innerHTML = json["name"];
+        temp.innerHTML = json["main"]["temp"] + " with " + json["weather"][0]["description"];
+        tempImg.src = "http://openweathermap.org/img/w/" + json["weather"][0]["icon"] + ".png";
+        tempImg.alt = json["weather"][0]["description"];
+        tempImg.title = json["weather"][0]["main"];
     });
+    forcast_div = document.querySelector("#forecast");
+    forcast_div.style.display = "block";
 }
